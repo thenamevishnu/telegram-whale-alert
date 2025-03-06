af5c14dcb69826081a6dad3777c3a84ab5e917c6
VISHNU M K <mail.vishnumk@gmail.com>
2025-03-06 13:06:31 +0530

    updated: whale alert

--- Handlers/whale.controller.mjs
+++ Handlers/whale.controller.mjs
@@ -5,15 +5,15 @@ import { readFileSync, unlink, unlinkSync, writeFileSync } from "fs"
 export const getLatestWhaleInfo = async () => {
     try {
         const key = process.env.API_KEY
-        const { data } = await axios.get(`https://api.clankapp.com/v2/explorer/tx?api_key=${key}&s_timestamp=desc&>_amount_usd=100000000`)
+        const { data } = await axios.get(`https://api.clankapp.com/v2/explorer/tx?api_key=${key}&s_timestamp=desc&>_amount_usd=10000000`)
         const response = data?.data?.[0]
         if (!response) return false
-        const { amount, symbol, amount_usd, from_owner, to_owner, date, blockchain, hash, unique_machine_id } = response
+        const { format_amount, symbol, format_amount_usd, from_owner, to_owner, date, blockchain, hash, unique_machine_id } = response
         const lastHash = readFileSync("./last_id.txt", { encoding: "utf-8" })
         if (lastHash === hash) return false
         unlinkSync("./last_id.txt")
         writeFileSync("./last_id.txt", hash)
-        const text = `💰${amount} #${symbol} (${amount_usd} USD) move from ${from_owner} to ${to_owner}\n\nDate : ${date}\nBlockchain : #${blockchain.toUpperCase()}\nSymbol : #${symbol}`
+        const text = `💷 <code>${format_amount}</code> #${symbol.toUpperCase()} ~ <code>$${format_amount_usd}</code> moved from <code>${from_owner}</code> to <code>${to_owner}</code>\n\nDate : <code>${date}</code>\nBlockchain : #${blockchain.toUpperCase()}\nSymbol : #${symbol.toUpperCase()}`
         const inline_keyboard = [
             [
                 {
@@ -22,7 +22,7 @@ export const getLatestWhaleInfo = async () => {
                 }
             ]
         ]
-        return await api.sendMessage(process.env.CHANNEL_ID, `<b>${text}</b>`, { reply_markup: { inline_keyboard } })
+        return await api.sendMessage(process.env.CHANNEL_ID, `<b>${text}</b>`, { parse_mode: "HTML", reply_markup: { inline_keyboard } })
     } catch (error) {
         return console.log(error.message)
     }
--- app.mjs
+++ app.mjs
@@ -1,18 +1,25 @@
 import cron from "node-cron"
 import { getLatestWhaleInfo } from "./Handlers/whale.controller.mjs"
 import e from "express"
+import axios from "axios"
 
 const app = e()
 
 app.get("/", (_req, res) => {
-    return res.send("Hello World")
+    return res.status(200).send("Hello World")
 })
 
 cron.schedule("* * * * *", async () => {
     try {
+        const response = await axios.get(process.env.API_URL)
+        console.log(response.data)
         const res = await getLatestWhaleInfo()
         if(res?.message_id) return console.log("Message sent successfully");
     } catch (error) {
         return console.log(error.message)
     }
+})
+
+app.listen(process.env.PORT || 8082, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8081}`)
 })
